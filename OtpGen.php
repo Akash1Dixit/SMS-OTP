a308cd4f1a04abdb6843bb03d5c8b630b263e921
Akash Dixit <anshudixit00011@gmail.com>
2018-09-11 13:09:59 +0530

    Delete My API&Hash

---
+++ OtpGen.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+<body>
+	  <div class="container" style="margin-top: 100px;">
+	     <div class="row justify-content-center">
+	     	<div class="col-md-6">
+	     		<div class="form-group">
+	     			<label for="">Name</label>
+	     			<input type="text" class="form-control" name="name" />
+	     		</div>
+	     	</div>
+	     </div>
+	     <div class="row justify-content-center">
+	     	<div class="col-md-6">
+	     		<div class="form-group">
+	     			<label for="">Mobile No.</label>
+	     			<input type="tel" class="form-control" name="mobile" />
+	     		</div>
+	     	</div>
+	     </div>	
+	      <div class="row justify-content-center">
+	     	<div class="col-md-6">
+	     		<div class="form-group">
+	     			<input type="submit" class="btn btn-primary" name="submit" />
+	     		</div>
+	     	</div>
+	     </div>
+	     <div class="row justify-content-center">
+	     	<div class="col-md-6">
+	     		<div class="form-group">
+	     			<label for="">OTP</label>
+	     			<input type="digit" name="otp_no" class="form-control" />
+	     		</div>
+	     	</div>
+	     </div>
+	  <div class="row justify-content-center">
+	     	<div class="col-md-6">
+	     		<div class="form-group">
+	     			<input type="submit" name="verify" class="btn btn-success" value="Verify" />
+	     		</div>
+	     	</div>
+	     </div>
+	</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"  crossorigin="anonymous"></script>
+</body>
+
+
+</html>
